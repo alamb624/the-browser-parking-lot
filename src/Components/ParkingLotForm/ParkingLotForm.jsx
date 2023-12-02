b51a0d1403fbb25e51cc8b6f696806e689008c7f
Allison Lamb <allison.lamb624@gmail.com>
2023-12-02 18:29:54 -0500

    controlled components

--- src/Components/ParkingLotForm/ParkingLotForm.jsx
+++ src/Components/ParkingLotForm/ParkingLotForm.jsx
@@ -1,12 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Form, Button, FormGroup, Label, Input } from 'reactstrap';
 
 import './ParkingLotForm.css'
 
 
+const PRIORITIES = {
+    Low: 'Low',
+    Medium: 'Medium',
+    High: 'High',
+}
+
 export default function ParkingLotForm(){
+
+    const [date, setDate] = useState('');
+    const[link, setLink] = useState('');
+    const[description, setDescription] = useState('');
+    const[priority, setPriority] = useState(PRIORITIES.Medium);
+
+    function handleDateChange(e) {
+        setDate(e.target.value);
+    }
+
+    function handleLinkChange(e) {
+        setLink(e.target.value);
+    }
+
+    function handleDescriptionChange(e) {
+        setDescription(e.target.value);
+    }
+
+    function handlePriorityChange(e) {
+        setPriority(e.target.value);
+    }
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        console.log(date, link, description, priority);
+    }
+
     return (
-        <Form data-bs-theme="dark" className="parking-lot-form">
+        <Form data-bs-theme="dark" className="parking-lot-form"
+    onSubmit={handleSubmit}>
             <FormGroup className="parking-lot-row">
                 <Label for="link-date">
                 Date
@@ -15,6 +49,8 @@ export default function ParkingLotForm(){
                 id="link-date"
                 name="date"
                 type="date"
+                value={date}
+                onChange={handleDateChange}
                 required
                 />
             </FormGroup>
@@ -26,6 +62,8 @@ export default function ParkingLotForm(){
                 id="link-url"
                 name="url"
                 type="url"
+                value={link}
+                onChange={handleLinkChange}
                 required
                 />
             </FormGroup>
@@ -37,6 +75,8 @@ export default function ParkingLotForm(){
                 id="link-description"
                 name="description"
                 type="text"
+                value={description}
+                onChange={handleDescriptionChange}
                 required
                 />
             </FormGroup>
@@ -46,6 +86,8 @@ export default function ParkingLotForm(){
                     type="radio"
                     value="high"
                     id="prio-high"
+                    checked={priority === PRIORITIES.High}
+                    onChange={handlePriorityChange}
                 />
                 {` `}
                 <Label for="prio-high" className="me-3">
@@ -56,6 +98,8 @@ export default function ParkingLotForm(){
                     type="radio"
                     value="medium"
                     id="prio-medium"
+                    checked={priority === PRIORITIES.Medium}
+                    onChange={handlePriorityChange}
                 />
                 {` `}
                 <Label for="prio-medium" className="me-3">
@@ -66,6 +110,8 @@ export default function ParkingLotForm(){
                     type="radio"
                     value="low"
                     id="prio-low"
+                    checked={priority === PRIORITIES.Low}
+                    onChange={handlePriorityChange}
                 />
                 {` `}
                 <Label for="prio-low" classname="me-3">
